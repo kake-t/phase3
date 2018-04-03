11c824a53ce2320b20b2d0718b6db6955a220277
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-04-03 19:14:37 +0900

    一覧表示

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,3 +1,21 @@
 class PicturesController < ApplicationController
   def top; end
+
+  def index
+  @pictures = Picture.all.order('created_at desc')
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+  def pictures_params
+    params.require(:picture).permit(:image, :title, :content, :user_name)
+  end
 end
