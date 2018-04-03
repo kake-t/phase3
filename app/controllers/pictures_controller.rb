6d449769fb71cce82fab153deccb3b95df47a21b
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-04-03 19:38:35 +0900

    make new

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,11 +1,13 @@
 class PicturesController < ApplicationController
+  before_action 
   def top; end
 
   def index
-  @pictures = Picture.all.order('created_at desc')
+    @pictures = Picture.all.order('created_at desc')
   end
 
   def new
+    @picture = Picture.new
   end
 
   def show
@@ -14,8 +16,17 @@ class PicturesController < ApplicationController
   def edit
   end
 
+  def create
+    @picture = Picture.new(pictures_params)
+    @picture.save
+    redirect_to pictures_path
+  end
+
   private
   def pictures_params
     params.require(:picture).permit(:image, :title, :content, :user_name)
+  end
+
+  def set_picture
   end
 end
