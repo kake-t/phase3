d73d86af44018c193da287b499d96782cb4d7cbd
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-04-08 00:03:58 +0900

    make session new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: %i[new create show]
+  resources :sessions, only: %i[new create destroy]
   resources :pictures do
     collection do
       post :confirm
