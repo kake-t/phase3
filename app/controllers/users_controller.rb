fd042f12e33d0bd5e4d7c73b0fc4da9929fe4019
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-04-07 23:30:06 +0900

    make users show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_params, only: %i[show destroy]
+
   def new
     @user = User.new
   end
@@ -6,11 +8,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path
+      redirect_to user_path(@user.id)
     else
       render 'new'
     end
   end
+
+  def show; end
 
   private
 
@@ -19,5 +23,9 @@ class UsersController < ApplicationController
                                  :email,
                                  :password,
                                  :password_confirmation)
+  end
+
+  def set_params
+    @user = User.find(params[:id])
   end
 end
