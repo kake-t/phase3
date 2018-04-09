c6556824434c3ab5b11b8e254c49ddae101eb6be
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-04-09 16:58:19 +0900

    お気に入り機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,11 @@
 class FavoritesController < ApplicationController
+  def create
+    favorite = current_user.favorites.create(picture_id: params[:picture_id])
+    redirect_to pictures_path, notice: "#{favorite.picture.user.name}さんの投稿をお気に入り登録しました"
+  end
+
+  def destroy
+    favorite = current_user.favorites.find_by(picture_id: params[:picture_id]).destroy
+    redirect_to pictures_path, notice: "お気に入りを解除しました"
+  end
 end
