02e604e0686c9fe7399c8a89399cafb2dabfb43e
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-04-09 16:03:54 +0900

    rails g controller favorite

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :favorites, only: %i[create destroy]
   resources :users, only: %i[new create show]
   resources :sessions, only: %i[new create destroy]
   resources :pictures do
