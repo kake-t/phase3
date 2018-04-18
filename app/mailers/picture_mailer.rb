02bcec70817582be126b0aa4169ce52437cdefb1
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-04-18 16:59:13 +0900

    投稿者にメール送信

--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -1,6 +1,6 @@
 class PictureMailer < ApplicationMailer
   def picture_mail(picture)
     @picture = picture
-    mail to: current_user.email, subject: '投稿完了'
+    mail to: @picture.user.email, subject: '投稿完了'
   end
 end
